f30c2bcb81392cc40144e6d1113df7280ea3a81a
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-04-03 15:08:09 +0530

    Sixth Commit

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
+
     path('create-room/', views.createroom, name='create-room'),
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
--- base/views.py
+++ base/views.py
@@ -78,7 +78,10 @@ def home(request):
         Q(host__username__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q)).order_by('-created')
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
@@ -102,6 +105,12 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages,
                'participants': participants}
     return render(request, 'base/room.html', context)
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
 
 
 @login_required(login_url="login")
